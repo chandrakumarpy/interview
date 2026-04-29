a1b57f33c0bd5408039e87ae3d13fad496983f1f
chandu <94897352+chandrakumarpy@users.noreply.github.com>
2026-04-29 20:50:13 +0530

    Add run-length encoding function
    Implement run-length encoding for a string.

--- string.py
+++ string.py
@@ -1,1 +1,10 @@
-
+# a = 'aaabbbxxdddaaccuuzzz' output = 3a3b2x3d2a2c2u3z
+m = ''
+count = 1
+for i in range(1,len(a)):
+    if a[i] == a[i-1]:
+        count += 1
+    else:
+        m += str(count)+a[i-1]
+        count = 1
+print(m+str(count)+a[-1])
