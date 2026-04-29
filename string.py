7a6222b33fa1683769f3405de5739bb9df6346c0
chandu <94897352+chandrakumarpy@users.noreply.github.com>
2026-04-29 20:54:58 +0530

    Update string.py

--- string.py
+++ string.py
@@ -8,3 +8,10 @@ for i in range(1,len(a)):
         m += str(count)+a[i-1]
         count = 1
 print(m+str(count)+a[-1])
+
+# count of ovels in string a = 'potulachandrakumar' output = {'o': 1, 'u': 2, 'a': 4}
+d={}
+for i in a:
+    if i in 'aeiou':
+        d[i]=d.get(i, 0)+1
+print(d)
